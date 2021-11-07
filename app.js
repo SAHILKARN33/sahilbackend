306cf41f20590c81d2a47d42e53d66933db7d2d0
awantikapradhan <wntkpradhan@gmail.com>
2021-11-07 16:23:10 +0530

    changes

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const router = require('./Router/index');
 
 const app = express();
 
-const PORT = process.env.port || 2001;
-//const host = 'localhost';
-//const dbUrl = 'mongodb://127.0.0.1:27017/zomato_clone'
-const serverdbUrl ='mongodb+srv://zomato_clone_user:zomatoclone1234@cluster0.dqvzn.mongodb.net/Zomato?retryWrites=true&w=majority'
+const host = '0.0.0.0';
+const port =process.env.PORT || 2001;
+//const dbUrl = 'mongodb://127.0.0.1:27017/zomato_17';
+const serverDBURL = 'mongodb+srv://zomato_clone_user:zomatoclone1234@cluster0.dqvzn.mongodb.net/Zomato?retryWrites=true&w=majority';
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+// CORS - Cross Origin Resource Sharing
 app.use(cors());
-app.use(express.json());
+app.options('*', cors());
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', 'content-type');
-	next();
-});
+app.use(express.json());
 
 app.use('/', router);
 
-mongoose
-	.connect(serverdbUrl, {
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-	})
-	.then((result) => {
-		console.log(`mongo db connected`);
-		app.listen(PORT, console.log(`Server is running at port ${PORT}`));
-	})
-	.catch((error) => console.log(error));
-
+mongoose.connect(serverDBURL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+    .then(res => {
+        app.listen(port, host, () => {
+            console.log(`Server is Running at ${host}:${port}`)
+        });
+    })
+    .catch(err => console.log(err));
 
